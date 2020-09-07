a5aed64e211bd305569dbaff78345047bb6b238f
Sachin Subramanian <sachin.subs@outlook.com>
2020-09-07 05:13:18 -0500

    ch01_pr01

--- ch01-matlab-exercises/ch01_pr01.m
+++ ch01-matlab-exercises/ch01_pr01.m
@@ -1,1 +1,18 @@
-rand(5)
+% Use MATLAB to generate random 4x4 matrices A and B. For each of the following,
+% compute A1, A2, A3, and A4 as indicated and determine which of the matrices
+% are equal (you can use MATLAB to test whether two matrices are equal by
+% computing their difference):
+% (a) A1 = A * B, A2 = B * A, A3 = (A' * B')', A4 = (B' * A')'
+% (b)
+% (c)
+% (d)
+
+A = rand(4)
+B = rand(4)
+
+% (a)
+
+A1 = A * B
+A2 = B * A
+A3 = (A' * B')'
+A4 = (B' * A')'
